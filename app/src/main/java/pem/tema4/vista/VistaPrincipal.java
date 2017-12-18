e7e21dc551ef9f465a56cba99c365d0413c1afe1
IvanGlez1996 <ivanglez1996@gmail.com>
2017-12-18 12:11:02 +0000

    Final commit

--- app/src/main/java/pem/tema4/vista/VistaPrincipal.java
+++ app/src/main/java/pem/tema4/vista/VistaPrincipal.java
@@ -458,8 +458,6 @@ public class VistaPrincipal extends AppCompatActivity implements IVistaPrincipal
 			// Handle the camera action
 		} else if (id == R.id.nav_gallery) {
 
-		} else if (id == R.id.nav_slideshow) {
-
 		} else if (id == R.id.nav_manage) {
 
 		} else if (id == R.id.nav_share) {
